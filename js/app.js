2a2179dbd97560e028db066bf7d690bc1107fbb1
gwens <gwenanspearing@hotmail.com>
2015-10-29 08:29:04 +0000

    basic switch statement with logs to start to build gem placement

--- js/app.js
+++ js/app.js
@@ -1,10 +1,53 @@
-////from engine.js:
-/*This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.*/
+//implementing levels... level should affect the speed of the bugs
+//and a number in the top-right of the screen
+//and reset the gems
+//after 10 levels should complete the game
 
-////need to randomise y position of new enemy. and timing/probability of new one appearing at any time? or have a constant number, some of which are off screen
-////keep track of amount of space enemy takes up, so they can't appear too close together? - no, they overlap in video (could add this feature though)
+//gems should appear randomly on the road at the beginning of each level
+//collecting a gem gives points that display in the top-right, next to a mini picture of a gem
+//points can be stored in the player object
+//gems can't be on top of each other
+//gems stored in a 5x3 array to represent the squares? 
+//Random number in each square, then cycle through and add relevant gem or object with a switch statement?
+
+//rows are at ROW * 83 - 30 (rows 1-3) or ROW * 83 + 53 (rows 0-2)
+//columns are at COLUMN * 100 (cols 1-5)
+//let's have heart, star, and 3 gems, and nothing, in proportions:
+//nothing: 9 0:49
+//blue gem: 3 50:69
+//green gem: 2 70:84
+//orange gem: 0.5 85:94
+//heart: 0.25 95:97
+//star: 0.25 98:99
+//random numbers from 0 to 99
+
+//this can be called by the player initalize function, or by a level initialize
+var setGems = function(){
+    for (var i = 0; i < 5; i++){ //iterate over columns
+        for (var j = 0; j < 3; j++){ //rows
+            var random = Math.floor(Math.random()*100);
+            switch(true){
+                case (random < 50):
+                break;
+                case (random < 70):
+                console.log("place blue gem at col" + i + "and row" + j);
+                break;
+                case (random < 85):
+                console.log("place green gem at col" + i + "and row" + j);
+                break;
+                case (random < 95):
+                console.log("place orange gem at col" + i + "and row" + j);
+                break;
+                case (random < 98):
+                console.log("place heart at col" + i + "and row" + j);
+                break;
+                case (random < 100): //could also use default but reads weirdly
+                console.log("place star at col" + i + "and row" + j);
+                break;
+            }
+        }
+    }
+};
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -15,7 +58,7 @@ var Enemy = function() {
 };
 
 Enemy.prototype.initialize = function(){
-    this.speed = Math.random() * 150 + 50;
+    this.speed = Math.random() * 1000 + 100; //make this a function of level? or make initialize a function of level
     this.x = -100;
     this.y = (Math.floor(Math.random() * 3 ) + 1) * 83 - 30; //choose random row
 };
@@ -116,6 +159,8 @@ function checkCollisions(){
 // Place the player object in a variable called player          ////
 
 //there are only three bugs on screen at any one time
+
+setGems();
 
 var bug1 = new Enemy();
 var bug2 = new Enemy();
